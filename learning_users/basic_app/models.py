a5f2e08e1565b696e69e4925c78e280b5338bf6f
Austin West <austin.west@dacumen.com>
2017-10-27 18:16:03 -0400

    'first'

---
+++ learning_users/basic_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+
+    # Create relationship (don't inherit from User!)
+    user = models.OneToOneField(User)
+
+    # Add any additional attributes you want
+    portfolio_site = models.URLField(blank=True) #user doesn't have to fill out
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        # Built-in attribute of django.contrib.auth.models.User !
+        return self.user.username
